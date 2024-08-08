4164fed50db3573c0c8fd623a641a3c91aab8f96
ChooJG <cnwprms@naver.com>
2024-08-08 17:08:39 +0900

    08/08 django study

--- third/views.py
+++ third/views.py
@@ -1,13 +1,17 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
-from third.models import Restaurant
-from third.forms import RestaurantForm
+from django.shortcuts import render, get_object_or_404, redirect
+from third.models import Restaurant, Review
+from third.forms import RestaurantForm, ReviewForm, UpdateRestaurantForm
 from django.http import HttpResponseRedirect
+from django.db.models import Count, Avg
+
+
 # Create your views here.
 
 
 def list(request):
-    restaurant = Restaurant.objects.all()
+    restaurant = (Restaurant.objects.all().annotate(reviews_count=Count('review'))
+                  .annotate(average_point=Avg('review__point')))
     paginator = Paginator(restaurant, 5)
 
     page = request.GET.get('page') # third/list?page=1
@@ -31,12 +35,65 @@ def create(request):
 
 def update(request):
     if request.method == "POST" and 'id' in request.POST:
-        item = Restaurant.objects.get(pk=request.POST.get('id'))
-        form = RestaurantForm(request.POST, instance=item)
-        if form.is_valid():
+        # item = Restaurant.objects.get(pk=request.POST.get('id'))
+
+        #에러가 안나오고, 페이지 없음이 나오게 함
+        item = get_object_or_404(Restaurant, pk=request.POST.get('id'))
+        password = request.POST.get('password', '')
+        form = UpdateRestaurantForm(request.POST, instance=item)
+        if form.is_valid() and password == item.password:
             item = form.save()
     elif request.method == 'GET':
-        item = Restaurant.objects.get(pk=request.GET.get('id'))
+        # item = Restaurant.objects.get(pk=request.GET.get('id'))
+
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
         form = RestaurantForm(instance=item)
         return render(request, 'third/update.html', {'form': form})
     return HttpResponseRedirect('/third/list/')
+
+
+def detail(request, id):
+    if id is not None:
+        item = get_object_or_404(Restaurant, pk=id)
+        reviews = Review.objects.filter(restaurant=item).all()
+        return render(request, 'third/detail.html', {'item':item, 'reviews': reviews})
+    return HttpResponseRedirect('/third/list/')
+
+
+def delete(request):
+    if 'id' in request.GET:
+        item = get_object_or_404(Restaurant, pk=request.GET.get('id'))
+        item.delete()
+    return HttpResponseRedirect('/third/list/')
+
+
+def review_create(request, restaurant_id):
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            new_item = form.save()
+        return redirect('restaurant-detail', id=restaurant_id)
+
+    item = get_object_or_404(Restaurant, pk=restaurant_id)
+    form = ReviewForm(initial={'restaurant': item})
+    return render(request, 'third/review_create.html', {'form': form, 'item': item})
+
+
+def review_delete(request, restaurant_id, review_id):
+    item = get_object_or_404(Review, pk=review_id)
+    item.delete()
+
+    return redirect('restaurant-detail', id=restaurant_id)
+
+
+def review_list(request):
+    reviews = Review.objects.all().select_related().order_by('-created_at')
+    paginator = Paginator(reviews, 10)
+
+    page = request.GET.get('page')
+    items = paginator.get_page(page)
+
+    context = {
+        'reviews': items
+    }
+    return render(request, 'third/review_list.html', context)
